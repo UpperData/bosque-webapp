6d5a3fa2a9873c9918e5141f42f36614560dd51d
ANGEL DIAZ <upper.venezuela@gmail.com>
2024-10-28 21:46:05 -0400

    removing build

--- src/configs/AppConfig.js
+++ src/configs/AppConfig.js
@@ -1,4 +1,4 @@
 export const APP_NAME           = 'Bosque marino';
-export const API_BASE_URL       = "http://127.0.0.1:4094"; // "http://192.168.1.3:4094"; // "http://147.135.93.82:4094"
+export const API_BASE_URL       = "http://localhost:4094"; // "http://192.168.1.3:4094"; // "http://147.135.93.82:4094"
 export const AUTH_PREFIX_PATH   = '/';
 export const APP_PREFIX_PATH    = '/dashboard';
