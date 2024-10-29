623b01b88608e3b825240ddc4c245d41648508ef
ANGEL DIAZ <upper.venezuela@gmail.com>
2024-10-29 09:28:12 -0400

    fix

--- src/configs/AppConfig.js
+++ src/configs/AppConfig.js
@@ -1,4 +1,4 @@
 export const APP_NAME           = 'Bosque marino';
-export const API_BASE_URL       = "https://tubosquemarino.com:4094"; 
+export const API_BASE_URL       = "//tubosquemarino.com:4094"; 
 export const AUTH_PREFIX_PATH   = '/';
 export const APP_PREFIX_PATH    = '/dashboard';
